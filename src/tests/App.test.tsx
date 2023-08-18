6b4cd0fe53a7c460710a3772396c24084e3c240e
Danilo <danilocbertolini@gmail.com>
2023-08-17 21:05:01 -0300

    Requisito de testes

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
 import { vi } from 'vitest';
 import { mockData } from './helpers/mockData';
+import DataProvider from '../context/DataProvider';
+import { tableKeys, optionsValuesArray } from './helpers/array';
+import userEvent from '@testing-library/user-event';
 
 afterEach(() => {
   vi.clearAllMocks();
@@ -11,13 +14,61 @@ beforeEach(async () => {
   global.fetch = vi.fn().mockResolvedValue({
     json: async () => (mockData),
   });
-  render(<App />)
+  render(
+    <DataProvider>
+      <App />
+    </DataProvider>
+  )
   expect(global.fetch).toBeCalledTimes(1);
 
 });
 
-test('I am your test', () => {
+test('Verifica se existe um Titulo', () => {
 
   const linkElement = screen.getByText(/Star Wars/i);
   expect(linkElement).toBeInTheDocument();
+
 });
+
+test('Verifica se depois do loading, existe a tabela', async () => {
+  const loading = await screen.findByText(/loading.../i);
+  expect(loading).toBeInTheDocument();
+
+  const table = await screen.findByRole('table')
+  expect(table).toBeInTheDocument();
+  expect(loading).not.toBeInTheDocument();  
+  
+  tableKeys.forEach((key) => {
+    screen.findByRole('columnheader', {  name: key})
+  })
+
+  const input = screen.getByRole('textbox')
+  userEvent.type(input, 'naboo')
+
+  const rows = await screen.findAllByRole('row');
+  expect(rows).toHaveLength(2)
+
+  const column = screen.getByTestId('column-filter');
+  const comparison = screen.getByTestId('comparison-filter');
+  const value = screen.getByTestId('value-filter');
+  const button = screen.getByTestId('button-filter');
+
+  userEvent.selectOptions(column, 'population');
+  userEvent.selectOptions(comparison, 'maior que');
+  userEvent.clear(value)
+  userEvent.type(value, '1000000');
+  userEvent.click(button);
+
+  userEvent.selectOptions(column, 'rotation_period');
+  userEvent.selectOptions(comparison, 'maior que');
+  userEvent.clear(value)
+  userEvent.type(value, '24');
+  userEvent.click(button);
+
+  const filterPopulation = await screen.findByText(/population maior que 1000000/i)
+  const filterRotation = await screen.findByText(/rotation_period maior que 24/i)
+
+  expect(filterPopulation).toBeInTheDocument();
+  expect(filterRotation).toBeInTheDocument();
+})
+
---
+++ src/tests/helpers/array.ts
@@ -0,0 +1,23 @@
+export const tableKeys = [
+  'Name',
+  'Rotation Period',
+  'Orbital Period',
+  'Diameter',
+  'Climate',
+  'Gravity',
+  'Terrain',
+  'Surface Water',
+  'Population',
+  'Films',
+  'Created',
+  'Edited',
+  'Url',
+];
+
+export const optionsValuesArray = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
